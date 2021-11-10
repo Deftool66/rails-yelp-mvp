8d6477660ff30500b3be00ce04ddceff9f7a0b20
Tarik Azale <deftool@gmx.net>
2021-11-10 12:01:45 +0100

    Restaurant model generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_10_105934) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "Adress"
+    t.string "phone_number"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
